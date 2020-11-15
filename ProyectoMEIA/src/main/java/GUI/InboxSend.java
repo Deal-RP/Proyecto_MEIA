d11e0918007858bc9a7d91e95cc082d01519ba4a
Pablo Muralles <pablomuralles28@gmail.com>
2020-11-15 00:14:50 -0600

    se implemento el mostrar los correo de la bandeja de salida

--- ProyectoMEIA/src/main/java/GUI/InboxSend.java
+++ ProyectoMEIA/src/main/java/GUI/InboxSend.java
@@ -5,17 +5,33 @@
  */
 package GUI;
 
+import Management.ABB;
+import Management.Data;
+import java.io.IOException;
+import java.util.List;
+import java.util.Vector;
+import java.util.regex.Pattern;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author pablo
  */
 public class InboxSend extends javax.swing.JFrame {
-
+    ABB ABB = new ABB();
     /**
      * Creates new form Inbox
      */
     public InboxSend() {
         initComponents();
+         try{
+            var dataUser = Data.getData();
+            var user = dataUser.getUser();
+            ABB.Cargar();
+            var tempList = ABB.display_msg_(user, 0);
+            ShowEmails( tempList);
+            }catch(IOException ex){
+            }
     }
 
     /**
@@ -28,31 +44,19 @@ public class InboxSend extends javax.swing.JFrame {
     private void initComponents() {
 
         jButton1 = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTable_Inbox = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jTextField_busqueda = new javax.swing.JTextField();
         jButton_buscar = new javax.swing.JButton();
         jButton_recargar = new javax.swing.JButton();
         jButton_salir = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable_Inbox = new javax.swing.JTable();
 
         jButton1.setText("jButton1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jTable_Inbox.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Receptor", "Asunto", "Mensaje", "Fecha"
-            }
-        ));
-        jScrollPane1.setViewportView(jTable_Inbox);
 
         jLabel1.setFont(new java.awt.Font("Tw Cen MT", 1, 18)); // NOI18N
         jLabel1.setText("Bandeja de Salida");
@@ -60,6 +64,11 @@ public class InboxSend extends javax.swing.JFrame {
         jLabel2.setText("Buscar Correo ");
 
         jButton_buscar.setText("Buscar");
+        jButton_buscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_buscarActionPerformed(evt);
+            }
+        });
 
         jButton_recargar.setText("Recargar");
         jButton_recargar.addActionListener(new java.awt.event.ActionListener() {
@@ -75,30 +84,40 @@ public class InboxSend extends javax.swing.JFrame {
             }
         });
 
+        jTable_Inbox.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Receptor", "Asunto", "Mensaje", "Fecha"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable_Inbox);
+
+        jScrollPane2.setViewportView(jScrollPane1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 964, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel1)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(37, 37, 37)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel1)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel2)
-                                .addGap(58, 58, 58)
-                                .addComponent(jTextField_busqueda, javax.swing.GroupLayout.PREFERRED_SIZE, 350, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(26, 26, 26)
-                        .addComponent(jButton_buscar, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(32, 32, 32)
-                        .addComponent(jButton_recargar)
-                        .addGap(18, 18, 18)
-                        .addComponent(jButton_salir, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                        .addComponent(jLabel2)
+                        .addGap(58, 58, 58)
+                        .addComponent(jTextField_busqueda, javax.swing.GroupLayout.PREFERRED_SIZE, 350, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(26, 26, 26)
+                .addComponent(jButton_buscar, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32)
+                .addComponent(jButton_recargar)
+                .addGap(18, 18, 18)
+                .addComponent(jButton_salir, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 992, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -113,8 +132,8 @@ public class InboxSend extends javax.swing.JFrame {
                     .addComponent(jButton_buscar)
                     .addComponent(jButton_recargar)
                     .addComponent(jButton_salir))
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 409, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 421, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -123,12 +142,28 @@ public class InboxSend extends javax.swing.JFrame {
 
     private void jButton_recargarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_recargarActionPerformed
         // TODO add your handling code here:
+        var dataUser = Data.getData();
+        var user = dataUser.getUser();
+        var strError = "";
+        
+       var List_msg = ABB.display_msg_(user, 0);
+       ShowEmails(List_msg);
     }//GEN-LAST:event_jButton_recargarActionPerformed
 
     private void jButton_salirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_salirActionPerformed
         // TODO add your handling code here:
         this.dispose();
     }//GEN-LAST:event_jButton_salirActionPerformed
+
+    private void jButton_buscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_buscarActionPerformed
+        // TODO add your handling code here:
+         var dataUser = Data.getData();
+        var user = dataUser.getUser();
+        var strError = "";
+        var Name =    jTextField_busqueda.getText();
+        var List_use = ABB.search_user_(user, Name,1,0);
+        ShowEmails(List_use);
+    }//GEN-LAST:event_jButton_buscarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -165,6 +200,32 @@ public class InboxSend extends javax.swing.JFrame {
             }
         });
     }
+    
+    //method to show te emails in the data grid view
+    //parameter: dataList -> de data of the tree that have the emails
+    public void ShowEmails(List<String> dataList){
+    
+        // the list return a register in this order: emisor,receptor,fecha,asunto,mensaje,estado,adjunto
+        DefaultTableModel model = (DefaultTableModel) jTable_Inbox.getModel();
+        model.setRowCount(0);
+
+        
+        for (int i = 0; i < dataList.size(); i++) {
+            
+            var tempString = (String) dataList.get(i);
+            var split = tempString.split(Pattern.quote("|"));
+            
+            var arrayTemp = new Vector();
+            arrayTemp.add(split[1]);
+            arrayTemp.add(split[3]);
+            arrayTemp.add(split[4]);
+            arrayTemp.add(split[2]);
+            
+            DefaultTableModel dt = (DefaultTableModel) jTable_Inbox.getModel();
+            dt.addRow(arrayTemp);
+        }
+    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
@@ -174,6 +235,7 @@ public class InboxSend extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable_Inbox;
     private javax.swing.JTextField jTextField_busqueda;
     // End of variables declaration//GEN-END:variables
